692682dfabe4c964f89124c7d558172b7dd5373a
MuchinaWamburia <lucymuchina20@gmail.com>
2022-11-30 09:08:09 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ const newTaskDescription = document.getElementById("new-task-description");
+ // we assign" tasks" an ID to avoid repeatedly typing it but instead just usong the varible
+ const theList = document.getElementById("tasks");  
+ 
+ const createNewTask = event => { 
+   event.preventDefault(); 
+   const newTask = document.createElement('li');
+ 
+   newTask.innerText = newTaskDescription.value;
+   createButton(newTask);
+   theList.appendChild(newTask);  // puts the child node at the end of the list
+ 
+   event.target.reset();
+ };
+ 
+ const createButton = task => {
+    const btn = document.createElement('button');
+     btn.innerText = "X";
+ 
+     task.appendChild(btn);
+     btn.addEventListener('click',  (e) => deleteTask(e));   //enable one to delete appended childNode
+ }
+ 
+ const deleteTask = (e) => {
+   const selectedTask = e.target.parentElement;
+   theList.removeChild(selectedTask);
+ }
+ 
+ document.querySelector('#create-task-form').addEventListener("submit"
+ , (e) => createNewTask(e))
